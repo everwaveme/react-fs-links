e8959289ca86f4d9bd16fc0c10490f92d6be2628
Farhad Sadykov <f.sadykov@yahoo.com>
2024-10-28 22:32:29 +0300

    added data js files

---
+++ src/data/dataSocial.js
@@ -0,0 +1,23 @@
+const dataSocial = [{
+    name: 'Telegram',
+    desc: 't.me/everwaveme',
+    icon: '../../assets/telegram-icon.png',
+    link: 'https://t.me/everwaveme'
+}, {
+    name: 'VK',
+    desc: 'vk.com/fsadykov',
+    icon: '../../assets/vk-icon.png',
+    link: 'https://vk.com/fsadykov'
+}, {
+    name: 'X',
+    desc: 'x.com/_everwave',
+    icon: '../../assets/x-icon.png',
+    link: 'https://x.com/_everwave'
+}, {
+    name: 'Email',
+    desc: 'purestate@yandex.ru',
+    icon: '../../assets/email-icon.png',
+    link: 'mailto:purestate@yandex.ru'
+}];
+
+export default dataSocial;
