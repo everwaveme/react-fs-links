663ab3aaf0dc3d9e879a12fbbf44100d3fe040f2
Farhad Sadykov <f.sadykov@yahoo.com>
2024-10-30 22:59:55 +0300

    started Tabs component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import CardInfo from './components/CardInfo';
+import Tabs from './components/Tabs';
 
 function App() {
   return (
     <div className="container">
       <div className="card-wrap">
         <CardInfo />
+        <Tabs />
       </div>
     </div>
   );
---
+++ src/components/Tabs/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Tabs() {
+
+    return (
+        <div className="card-tabs">
+            <button className="card-tabs-social active">
+                Social
+            </button>
+            <button className="card-tabs-dev">
+                Dev
+            </button>
+            <button className="card-tabs-photography">
+                Photography
+            </button>
+        </div>
+    
+        
+    );
+}
+
+export default Tabs;
