c3f79561991dec4e92938a2cf2464d70fe450e2e
Farhad Sadykov <f.sadykov@yahoo.com>
2024-11-11 22:49:08 +0300

    small fixes

---
+++ README.md
@@ -0,0 +1,3 @@
+Social, dev portfolio and photography links website on React.
+
+<img src="/main-screenshot.png" alt="main preview" width="100%"/>
--- src/data/dataDev.js
+++ src/data/dataDev.js
@@ -12,42 +12,48 @@ const dataDev = [{
     link: 'https://epic.everwave.ru/'
 }, {
     id: '3',
+    name: 'Links App',
+    desc: 'Social, dev portfolio and photography links app on React',
+    icon: 'react-icon.png',
+    link: 'https://github.com/everwaveme/react-fs-links'
+}, {
+    id: '4',
     name: 'Message History',
     desc: 'React component',
     icon: 'react-icon.png',
     link: 'https://github.com/everwaveme/message-history-react-component'
 }, {
-    id: '4',
+    id: '5',
     name: 'Modern Color',
     desc: 'Photography podcast',
     icon: 'web-icon.png',
     link: 'https://podcast.moderncolor.ru/'
 }, {
-    id: '5',
+    id: '6',
     name: 'Snooker Quiz',
     desc: '20 question quiz for snooker fans',
     icon: 'web-icon.png',
     link: 'https://q.homeofsnooker.ru/'
 }, {
-    id: '6',
+    id: '7',
     name: 'US Stocks Portfolio',
     desc: 'Stocks real-time price',
     icon: 'web-icon.png',
     link: 'https://github.com/everwaveme/js-stocks-real-time-price'
 }, {
-    id: '7',
+    id: '8',
     name: 'Mark Selby',
     desc: '4-time World Snooker Champion',
     icon: 'joker-icon.png',
     link: 'https://markselby.ru/'
 }, {
-    id: '8',
+    id: '9',
     name: 'Github repos list',
     desc: 'Getting a list of GitHub repositories by entering a username',
     icon: 'web-icon.png',
     link: 'https://github.com/everwaveme/js-get-github-repos-list/'
 }, {
-    id: '9',
+    id: '10',
     name: 'Farhad Sadykov',
     desc: 'Photography website',
     icon: 'web-icon.png',
